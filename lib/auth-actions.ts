87ac25cdea4d1ab556c6521269bacffa02b1213c
Maanav Chellani <maanav.chellani@gmail.com>
2025-10-22 16:17:10 -0400

    redirect to trips after sign-in

--- lib/auth-actions.ts
+++ lib/auth-actions.ts
@@ -3,7 +3,7 @@
 import { signIn, signOut } from "@/auth";
 
 export const login = async () => {
-  await signIn("github", { redirectTo: "/" });
+  await signIn("github", { redirectTo: "/trips" });
 };
 
 export const logout = async () => {
